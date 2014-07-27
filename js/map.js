807932507f44d9e0f7fbc89fff2eb6904fab7e57
Éric Gillet <eric.gillet+gh@linuxw.info>
2014-07-27 20:16:57 +0100

    download filters and auto way reversal

--- js/map.js
+++ js/map.js
@@ -32,11 +32,20 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var dlBbox = function() {
 	var bounds = map.getBounds();
 	var bbox = [bounds.getSouth(), bounds.getWest(), bounds.getNorth(), bounds.getEast()].join();
-	// $.ajax(opapi, {
-	// 	type: "POST",
-	// 	data: "[timeout:25][out:json];rel[type=route][network=TCL];(._;>;);out;",
-	// }).done(parseData);
-	$.ajax('./data/map.json').done(parseData);
+	var op = $('#opSelect').val();
+	var net = $('#netSelect').val();
+	var ref = $('#refSelect').val();
+
+	var netstr = net ? ("[network=" + net + "]") : "";
+	var opstr = op ? ("[operator=" + op + "]") : "";
+	var refstr = ref ? ("[ref=" + ref + "]") : "";
+
+	var query = "[timeout:45][out:json];rel[type=route](" + bbox + ")" + netstr + opstr + refstr + ";(._;>;);out;";
+	$.ajax(opapi, {
+		type: "POST",
+		data: query,
+	}).done(parseData);
+	// $.ajax('./data/map.json').done(parseData);
 };
 
 var parseData = function(data) {
@@ -59,10 +68,10 @@ var parseData = function(data) {
 	var netSelect = $("#netSelect");
 	var opSelect = $("#opSelect");
 	$.each(networks, function() {
-	    netSelect.append($("<option />").val(this).text(this));
+	    netSelect.append($("<option />").val(this));
 	});
 	$.each(operators, function() {
-	    opSelect.append($("<option />").val(this).text(this));
+	    opSelect.append($("<option>").val(this));
 	});
 
 	// Join nodes to ways
@@ -93,7 +102,7 @@ var parseData = function(data) {
 
 	for (r in rels) {
 		var path = [];
-		rels[r].members.forEach(function(m) {
+		rels[r].members.forEach(function(m, mn) {
 			switch(m.role) {
 				case "stop":
 				case "stop_entry_only":
@@ -105,10 +114,47 @@ var parseData = function(data) {
 					).addTo(map);
 					break;
 				case "":
-					if(m.type == "way") {
-						m.obj.nodes.forEach(function(n) {
-							path.push(L.latLng(n.lat, n.lon));
-						});
+					if(m.type != "way") return;
+
+					// Reverse way display if necessary
+					if(path.length) {
+						var lastNodePath = path[path.length - 1];
+						var firstNodeWay = L.latLng(m.obj.nodes[0].lat, m.obj.nodes[0].lon);
+						var lastNodeWay = L.latLng(m.obj.nodes[m.obj.nodes.length-1].lat, m.obj.nodes[m.obj.nodes.length-1].lon);
+						if(lastNodePath.distanceTo(firstNodeWay) < lastNodePath.distanceTo(lastNodeWay)) {
+							for(var i=0; i < m.obj.nodes.length; ++i) {
+								path.push(L.latLng(m.obj.nodes[i].lat, m.obj.nodes[i].lon));
+							}
+						} else {
+							for(var i=m.obj.nodes.length -1; i >= 0 ; --i) {
+								path.push(L.latLng(m.obj.nodes[i].lat, m.obj.nodes[i].lon));
+							}
+						}
+					} else {
+						// First route segment, try to guess orientation according to the next route segment.
+						var firstNodeFirstWay = m.obj.nodes[m.obj.nodes.length - 1];
+						var lastNodeFirstWay = m.obj.nodes[0];
+						//Find next route segment
+						var j;
+						for(j=mn; j<rels[r].members.length; ++i) {
+							if(rels[r].members[j].role == "" && rels[r].members[j].type == "way") {
+								break;
+							}
+						}
+						var firstNodeSecondWay = rels[r].members[j].obj.nodes[0];
+						
+						var distLast = L.latLng(lastNodeFirstWay.lat, lastNodeFirstWay.lon).distanceTo(L.latLng(firstNodeSecondWay.lat, firstNodeSecondWay.lon));
+						var distFirst = L.latLng(firstNodeFirstWay.lat, firstNodeFirstWay.lon).distanceTo(L.latLng(firstNodeSecondWay.lat, firstNodeSecondWay.lon));
+						
+						if(distLast > distFirst) {
+							for(var i=0; i < m.obj.nodes.length; ++i) {
+								path.push(L.latLng(m.obj.nodes[i].lat, m.obj.nodes[i].lon));
+							}
+						} else {
+							for(var i=m.obj.nodes.length -1; i >= 0 ; --i) {
+								path.push(L.latLng(m.obj.nodes[i].lat, m.obj.nodes[i].lon));
+							}
+						}
 					}
 					break;
 			}
